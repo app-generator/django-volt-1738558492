67a183ca82503f4aa1b394f3cbca5b3ff0eb830a
App Generator <51070104+app-generator@users.noreply.github.com>
2025-02-03 06:55:00 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,56 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Product(models.Model):
+
+    #__Product_FIELDS__
+    name = models.CharField(max_length=255, null=True, blank=True)
+    price = models.IntegerField(null=True, blank=True)
+    typeid = models.IntegerField(null=True, blank=True)
+
+    #__Product_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Product")
+        verbose_name_plural = _("Product")
+
+
+class Type(models.Model):
+
+    #__Type_FIELDS__
+    type = models.TextField(max_length=255, null=True, blank=True)
+
+    #__Type_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Type")
+        verbose_name_plural = _("Type")
+
+
+
+#__MODELS__END
